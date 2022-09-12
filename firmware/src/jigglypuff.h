2c7984833dfcb2f39e112c5be03e1cee1d167ab2
LidiaDomingos <72155007+LidiaDomingos@users.noreply.github.com>
2022-09-11 22:42:25 -0300

    mais musicas

---
+++ firmware/src/jigglypuff.h
@@ -0,0 +1,37 @@
+
+/*
+ * jigglypuff.h
+ *
+ * Created: 11/09/2022 22:03:54
+ *  Author: lidia
+ */ 
+
+#include <asf.h>
+#include "notes.h"
+
+#ifndef JIGGLYPUFF_H_
+#define JIGGLYPUFF_H_
+
+int melody_jigglypuff[] = {
+
+	// Jigglypuff's Song
+	// Score available at https://musescore.com/user/28109683/scores/5044153
+	
+	NOTE_D5,-4, NOTE_A5,8, NOTE_FS5,8, NOTE_D5,8,
+	NOTE_E5,-4, NOTE_FS5,8, NOTE_G5,4,
+	NOTE_FS5,-4, NOTE_E5,8, NOTE_FS5,4,
+	NOTE_D5,-2,
+	NOTE_D5,-4, NOTE_A5,8, NOTE_FS5,8, NOTE_D5,8,
+	NOTE_E5,-4, NOTE_FS5,8, NOTE_G5,4,
+	NOTE_FS5,-1,
+	NOTE_D5,-4, NOTE_A5,8, NOTE_FS5,8, NOTE_D5,8,
+	NOTE_E5,-4, NOTE_FS5,8, NOTE_G5,4,
+	
+	NOTE_FS5,-4, NOTE_E5,8, NOTE_FS5,4,
+	NOTE_D5,-2,
+	NOTE_D5,-4, NOTE_A5,8, NOTE_FS5,8, NOTE_D5,8,
+	NOTE_E5,-4, NOTE_FS5,8, NOTE_G5,4,
+	NOTE_FS5,-1,
+	
+};
+#endif /* JIGGLYPUFF_H_ */
---
+++ firmware/src/oled/happybirthday.h
@@ -0,0 +1,25 @@
+
+/*
+ * happybirthday.h
+ *
+ * Created: 11/09/2022 22:25:57
+ *  Author: lidia
+ */ 
+
+int melody_happybirthday[] = {
+
+	// Happy Birthday
+	// Score available at https://musescore.com/user/8221/scores/26906
+
+	NOTE_C4,4, NOTE_C4,8,
+	NOTE_D4,-4, NOTE_C4,-4, NOTE_F4,-4,
+	NOTE_E4,-2, NOTE_C4,4, NOTE_C4,8,
+	NOTE_D4,-4, NOTE_C4,-4, NOTE_G4,-4,
+	NOTE_F4,-2, NOTE_C4,4, NOTE_C4,8,
+
+	NOTE_C5,-4, NOTE_A4,-4, NOTE_F4,-4,
+	NOTE_E4,-4, NOTE_D4,-4, NOTE_AS4,4, NOTE_AS4,8,
+	NOTE_A4,-4, NOTE_F4,-4, NOTE_G4,-4,
+	NOTE_F4,-2,
+	
+};
